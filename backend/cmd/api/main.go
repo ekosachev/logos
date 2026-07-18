4177c35648a876e9707fe1e65795142fdb1ddfd6
Egor Kosachev <egorkosachev@vk.com>
2026-07-19 00:23:40 +0700

    feat(back): add database connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,13 +1,22 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/ekosachev/logos/internal/database"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	_, err := database.ConnectToDb()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.Default()
 	router.GET("/ping", func(ctx *gin.Context) { ctx.JSON(200, gin.H{"message": "pong"}) })
 
-	err := router.Run()
-	if err != nil {
-		panic(err)
+	if err = router.Run(); err != nil {
+		log.Fatal(err)
 	}
 }
---
+++ backend/internal/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"github.com/ekosachev/logos/internal/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectToDb() (*gorm.DB, error) {
+	cfg := config.GetConfig()
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		cfg.DBHost,
+		cfg.DBUser,
+		cfg.DBPassword,
+		cfg.DBName,
+		cfg.DBPort,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate()
+
+	return db, nil
+}
