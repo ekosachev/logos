1a9837d129403a700ef5354f9024f0fc2f30856d
Egor Kosachev <egorkosachev@vk.com>
2026-07-20 18:35:07 +0700

    feat(back:user): add user service to main

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,14 +4,19 @@ import (
 	"log"
 
 	"github.com/ekosachev/logos/internal/database"
+	"github.com/ekosachev/logos/internal/repositories"
+	"github.com/ekosachev/logos/internal/services"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	_, err := database.ConnectToDb()
+	db, err := database.ConnectToDb()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	userRepository := repositories.NewUserRepository(db)
+	_ = services.NewUserService(userRepository)
 
 	router := gin.Default()
 	router.GET("/ping", func(ctx *gin.Context) { ctx.JSON(200, gin.H{"message": "pong"}) })
