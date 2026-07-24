2ae6a71c94fcc7f3a7708acf5159f19b880b59d4
Egor Kosachev <egorkosachev@vk.com>
2026-07-24 21:30:24 +0700

    feat(back:auth): add middleware to check access token

--- backend/internal/services/middleware.go
+++ backend/internal/services/middleware.go
@@ -32,23 +32,37 @@ func verifyRefreshToken(tokenString string) (bool, *uuid.UUID) {
 	return false, nil
 }
 
+func verifyAccessToken(tokenString string) (bool, *uuid.UUID) {
+	cfg := config.GetConfig()
+	secretKey := cfg.JWTAccessSecret
+
+	token, _ := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) { return []byte(secretKey), nil })
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if userId, err := uuid.Parse(claims["sub"].(string)); err == nil {
+			return true, &userId
+		}
+	}
+
+	return false, nil
+}
+
 func (s *MiddlewareService) RequiresRefreshToken() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		tokenString, err := ctx.Cookie("refreshToken")
 		if err != nil {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"Success": false, "Error": "refreshToken cookie required"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"success": false, "error": "refreshToken cookie required"})
 			return
 		}
 
 		if ok, userID := verifyRefreshToken(tokenString); ok && userID != nil {
 			validToken, err := s.refreshTokenRepository.GetActiveRefreshToken(ctx, *userID)
 			if err != nil || validToken == nil {
-				ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"Success": false, "Error": "Unauthorized"})
+				ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"success": false, "error": "Unauthorized"})
 				return
 			}
 
 			if validToken.Token != tokenString {
-				ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"Success": false, "Error": "Unauthorized"})
+				ctx.AbortWithStatusJSON(http.StatusForbidden, gin.H{"success": false, "error": "Unauthorized"})
 				return
 			}
 
@@ -56,7 +70,26 @@ func (s *MiddlewareService) RequiresRefreshToken() gin.HandlerFunc {
 			ctx.Set("tokenID", validToken.ID.String())
 			ctx.Next()
 		} else {
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"Success": false, "Error": "Invalid token"})
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"success": false, "error": "Invalid token"})
+		}
+	}
+}
+
+func (s *MiddlewareService) RequiresAccessToken() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"success": false, "error": "Authorization header required"})
+			return
+		}
+
+		tokenString := authHeader[7:] // remove "Bearer: " prefix
+
+		if ok, userID := verifyAccessToken(tokenString); ok && userID != nil {
+			ctx.Set("userID", userID.String())
+			ctx.Next()
+		} else {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"success": false, "error": "Invalid token"})
 		}
 	}
 }
